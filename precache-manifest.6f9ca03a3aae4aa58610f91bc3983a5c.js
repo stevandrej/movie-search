940a69df46fddd514c81621efb46bd230752c67c
Andrej Stevkovski <stevandrej@yahoo.com>
2020-05-21 04:52:10 +0200

    upload

---
+++ precache-manifest.6f9ca03a3aae4aa58610f91bc3983a5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816732e3540ce53e7a867834c71c45e6",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "de27ef9ad2d29c51859c",
+    "url": "/movie-search/static/css/main.9d971d2d.chunk.css"
+  },
+  {
+    "revision": "1c3035c193932872ce26",
+    "url": "/movie-search/static/js/2.7ffd7640.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/movie-search/static/js/2.7ffd7640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de27ef9ad2d29c51859c",
+    "url": "/movie-search/static/js/main.72c3cd9b.chunk.js"
+  },
+  {
+    "revision": "9aa344bdc0fb76155026",
+    "url": "/movie-search/static/js/runtime-main.611718a1.js"
+  }
+]);
